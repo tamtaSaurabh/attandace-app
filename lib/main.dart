00f0675b866b0d0876ecf3992ec2f68ee3078b21
ADM Velocity FE <adwicorp+velocity@gmail.com>
2024-09-22 20:49:25 +0530

    added firebase authenticationa and dummy users

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
-import 'menu_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'screens/menu_screen.dart';
+import 'screens/login_screen.dart'; // Import login screen
+import 'screens/registration_screen.dart'; // Import registration screen
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure Flutter bindings are initialized
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Attendance App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MenuScreen(),
+      home: LoginScreen(), // Start with the Login Screen
     );
   }
 }
--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,43 +1,119 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:attendance_app/services/auth_services.dart';
+import 'login_screen.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:attendance_app/utils/constants.dart';
 
 class MenuScreen extends StatelessWidget {
+  final AuthService _authService = AuthService();
+
+  Future<List<dynamic>> loadMembers() async {
+    // Load the JSON file
+    final String response =
+        await rootBundle.loadString('lib/assets/fake_users.json');
+    final data = await json.decode(response);
+    return data['members'];
+  }
+
   @override
   Widget build(BuildContext context) {
+    User? currentUser = _authService.getCurrentUser();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Attendance App'),
+        title: Text(
+          'ATTENDANCE',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: AppColors.background,
+        iconTheme: IconThemeData(color: Colors.white),
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              child: Text('Menu',
-                  style: TextStyle(color: Colors.white, fontSize: 24)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.person, // Replace with your desired icon
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  SizedBox(height: 8), // Space between icon and text
+                  Text(
+                    currentUser?.displayName ?? "User",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    currentUser?.email ?? "",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ],
+              ),
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: AppColors.background,
               ),
             ),
             ListTile(
-              title: Text('Attendance'),
+              title: Text('Attendance',
+                  style: TextStyle(color: AppColors.textColor)),
               onTap: () {
                 // Navigate to Attendance Screen
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              title: Text('Locations'),
+              title: Text('Locations',
+                  style: TextStyle(color: AppColors.textColor)),
               onTap: () {
                 // Navigate to Locations Screen
                 Navigator.pop(context);
               },
             ),
+            ListTile(
+              leading: Icon(
+                Icons.logout,
+                color: AppColors.errorColor,
+              ),
+              title: Text('Sign Out',
+                  style: TextStyle(color: AppColors.textColor)),
+              onTap: () async {
+                await _authService.signOut();
+                Get.offAll(LoginScreen());
+              },
+            ),
           ],
         ),
       ),
-      body: Center(
-        child: Text('Welcome to Attendance App!'),
+      body: FutureBuilder<List<dynamic>>(
+        future: loadMembers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final members = snapshot.data!;
+            return ListView.builder(
+              itemCount: members.length,
+              itemBuilder: (context, index) {
+                final member = members[index];
+                return ListTile(
+                  title: Text(member['name']),
+                  subtitle: Text(member['email']),
+                  onTap: () {
+                    // Handle member tap (e.g., navigate to details screen)
+                  },
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
